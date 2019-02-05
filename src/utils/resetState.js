2c5b5310123a6284a04eb7690128664ce96c1a31
hon2a <jkonopasek@wiser.com>
2019-02-05 15:23:48 +0100

    Fixed `resetState` to properly unwrap the part of the state to be reset before setting it.

--- src/utils/resetState.js
+++ src/utils/resetState.js
@@ -1,4 +1,4 @@
-import { setIn } from '@hon2a/icepick-fp'
+import { getIn, setIn } from '@hon2a/icepick-fp'
 
 export const STATE_RESET = '__reducks.STATE_RESET__'
 export const resetState = (path, resetType) => ({ reducer, ...rest }) => ({
@@ -6,7 +6,7 @@ export const resetState = (path, resetType) => ({ reducer, ...rest }) => ({
     if (action.type !== resetType) return reducer(state, action)
 
     const defaults = reducer(undefined, { type: STATE_RESET })
-    return reducer(setIn(path, defaults)(state), action)
+    return reducer(setIn(path, getIn(path)(defaults))(state), action)
   },
   ...rest
 })
