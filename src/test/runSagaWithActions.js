44c043b79a77a159d054115160a467807c284883
Elad Shahar <SaladFork@Gmail.com>
2019-04-11 14:44:44 -0400

    Update runSaga usage to 1.0 API
    There's no test for this but I tried to keep the existing behavior. Used
    the docs as a reference:
    
      <https://github.com/redux-saga/redux-saga/blob/cb63a9/docs/advanced/UsingRunSaga.md>

--- src/test/runSagaWithActions.js
+++ src/test/runSagaWithActions.js
@@ -1,22 +1,22 @@
-import { runSaga } from 'redux-saga'
+import EventEmitter from 'events'
+import { runSaga, stdChannel } from 'redux-saga'
 
 export async function runSagaWithActions(saga, getState = () => {}, ...actions) {
   const dispatched = []
-  let handleAction
-  await runSaga(
-    {
-      subscribe: callback => {
-        handleAction = callback
-        actions.forEach(handleAction)
-        return () => {}
-      },
-      dispatch: action => {
-        dispatched.push(action)
-        handleAction(action)
-      },
-      getState
-    },
-    saga
-  )
+
+  const emitter = new EventEmitter()
+  const channel = stdChannel()
+  emitter.on('action', channel.put)
+
+  const testIO = {
+    channel,
+    dispatch: action => emitter.emit('action', action) && dispatched.push(action),
+    getState
+  }
+
+  await runSaga(testIO, saga)
+
+  actions.forEach(action => testIO.dispatch(action))
+
   return dispatched
 }
