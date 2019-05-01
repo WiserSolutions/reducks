b509caaba87ee96e1b64d0e5f56f62405bd90d05
hon2a <jkonopasek@wiser.com>
2019-05-01 12:07:04 +0200

    Created `sideEffectsMapSaga` (abstracted from more specific `notificationsSaga` in `pricing-application`) and added some tests.

---
+++ src/sagas/sideEffectsMapSaga.test.js
@@ -0,0 +1,23 @@
+import { sideEffectsMapSaga } from './sideEffectsMapSaga'
+import { runSagaWithActions } from '../test'
+
+describe('asyncActionSaga', () => {
+  const FIRST_TYPE = 'FIRST_TYPE'
+  const SECOND_TYPE = 'SECOND_TYPE'
+  const effect = jest.fn()
+  const saga = sideEffectsMapSaga({
+    [FIRST_TYPE]: () => effect('first'),
+    [SECOND_TYPE]: (payload, action, state) => effect('second', payload, action, state)
+  })
+  const state = { dummy: 'state' }
+
+  beforeEach(() => {
+    effect.mockReset()
+  })
+
+  it('maps actions to side-effect calls', async () => {
+    const action = { type: SECOND_TYPE, payload: 'test payload' }
+    await runSagaWithActions(saga, () => state, { type: FIRST_TYPE }, { type: 'IGNORED' }, action)
+    expect(effect.mock.calls).toEqual([['first'], ['second', action.payload, action, state]])
+  })
+})
