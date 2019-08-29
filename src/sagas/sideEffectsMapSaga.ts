7b054228ce4e018f45e3695ea1f06bc4023dd66f
hon2a <jkonopasek@wiser.com>
2019-08-29 16:57:15 +0200

    Added type annotations to `sideEffectsMapSaga`.

--- src/sagas/sideEffectsMapSaga.ts
+++ src/sagas/sideEffectsMapSaga.ts
@@ -1,11 +1,13 @@
 import { takeEvery, select, call } from 'redux-saga/effects'
 
-export const sideEffectsMapSaga = typeToSideEffectMap =>
+import { Message, ActionType } from '../types'
+
+export const sideEffectsMapSaga = (typeToSideEffectMap: Record<ActionType, (payload: any, message: Message, state: any) => any>) =>
   function*() {
-    yield takeEvery(Object.keys(typeToSideEffectMap), function* performSideEffect(action) {
-      const { type, payload } = action
+    yield takeEvery(Object.keys(typeToSideEffectMap), function* performSideEffect(message: Message) {
+      const { type, payload } = message
       const sideEffect = typeToSideEffectMap[type]
       const stateIfNeeded = sideEffect.length > 2 ? yield select() : undefined
-      yield call(sideEffect, payload, action, stateIfNeeded)
+      yield call(sideEffect, payload, message, stateIfNeeded)
     })
   }
