96ca70768468fc21038346384b0a91f1a7d90cb9
hon2a <jkonopasek@wiser.com>
2019-08-29 16:56:18 +0200

    Added type annotations to `utils/resetState`.

--- src/utils/resetState.ts
+++ src/utils/resetState.ts
@@ -1,12 +1,14 @@
 import { getIn, setIn } from '@hon2a/icepick-fp'
 
+import { Path, ActionType, Duck, Message } from '../types'
+
 export const STATE_RESET = '__reducks.STATE_RESET__'
-export const resetState = (path, resetType) => ({ reducer, ...rest }) => ({
-  reducer: (state, action) => {
-    if (action.type !== resetType) return reducer(state, action)
+export const resetState = <State>(path: Path, resetType: ActionType) => ({ reducer, ...rest }: Duck<State>) => ({
+  reducer: reducer && ((state: State, message: Message) => {
+    if (message.type !== resetType) return reducer(state, message)
 
     const defaults = reducer(undefined, { type: STATE_RESET })
-    return reducer(setIn(path, getIn(path)(defaults))(state), action)
-  },
+    return reducer(setIn(path, getIn(path)(defaults))(state), message)
+  }),
   ...rest
 })
