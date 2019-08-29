15bc3478ee91bfe2737694b25b83e9b114cc5725
hon2a <jkonopasek@wiser.com>
2019-08-29 11:16:01 +0200

    Added TypeScript as dependency and started updating tasks, Babel config, and TypeScript config.

--- .babelrc.js
+++ .babelrc.js
@@ -1,1 +1,10 @@
-module.exports = require('@wisersolutions/transpile-js/babel.config')
+const createDefaultConfig = require('@wisersolutions/transpile-js/babel.config')
+
+module.exports = api => {
+  const defaultConfig = createDefaultConfig(api)
+  return {
+    ...defaultConfig,
+    presets: [...defaultConfig.presets, '@babel/preset-typescript'],
+    plugins: [...defaultConfig.plugins, '@babel/proposal-object-rest-spread']
+  }
+}
