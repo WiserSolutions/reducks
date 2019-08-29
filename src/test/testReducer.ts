7b767a9f7a141db8ed76faff32508bf744ce4712
hon2a <jkonopasek@wiser.com>
2019-08-29 16:56:34 +0200

    Added type annotations to `test/testReducer`.

--- src/test/testReducer.ts
+++ src/test/testReducer.ts
@@ -1,17 +1,21 @@
+import { Reducer } from 'redux'
+
+import { Message, Selector } from '../types'
+
 /* eslint-env jest */
-export function testReducerChanges(reducer, initialState) {
+export function testReducerChanges<State>(reducer: Reducer<State>, initialState: State) {
   let state = initialState
-  return (action, expectedChange) => {
+  return (message: Message, expectedChange: (prevState: State) => State) => {
     const expectedState = expectedChange(state)
-    expect(reducer(state, action)).toEqual(expectedState)
+    expect(reducer(state, message)).toEqual(expectedState)
     state = expectedState
   }
 }
 
-export function testReducerSelector(reducer, selector, initialState) {
+export function testReducerSelector<State, Selection>(reducer: Reducer<State>, selector: Selector<State, Selection>, initialState: State) {
   let state = initialState
-  return (action, expectedValue) => {
-    const newState = reducer(state, action)
+  return (message: Message, expectedValue: any) => {
+    const newState = reducer(state, message)
     expect(selector(newState)).toEqual(expectedValue)
     state = newState
   }
