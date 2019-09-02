fa2593fc13748b06a32d335788afaa48ed35d6df
hon2a <jkonopasek@wiser.com>
2019-09-02 13:11:04 +0200

    Added type annotation to `helpers` and its test.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,8 @@
 import isFunction from 'lodash.isfunction'
 
-export const resolve = valueOrGetter => (...args) => {
+export const resolve = <Value, Input extends any[]>(valueOrGetter: Value | ((...args: Input) => Value)) => (
+  ...args: Input
+): Value => {
   if (isFunction(valueOrGetter)) {
     return valueOrGetter(...args)
   }
